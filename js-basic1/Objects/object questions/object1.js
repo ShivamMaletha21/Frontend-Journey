94ca3ebf490a66de39935757be33121c0d57635b
ShivamMaletha21 <malethashivam43@gmail.com>
2026-02-19 09:41:41 +0530

    object questons

---
+++ js-basic1/Objects/object questions/object1.js
@@ -0,0 +1,66 @@
+// Q1. Create an object car with properties:
+// brand: "BMW"
+// color: "Black"
+// price: 5000000
+// Print:
+// brand using dot notation
+// color using bracket notation 
+
+let car = {
+    brand: "BMW",
+    color: "Black",
+    price: 5000000,
+
+    detail: function showDetails() { // creating method
+        console.log(this.brand + " cost " +  this.price); // instead of writng BMW cost 4500000 we use this
+    },
+
+    discount: function applyDiscount() {
+        this.price = this.price - (this.price * 10 / 100);
+    }
+};
+
+console.log(car.brand);
+console.log(car["color"]);
+console.log(car.price);
+
+// bracket notation is used to acess properties dynamically
+
+// let profile = {
+//     email : "shiv123@gmail.com",
+//     city : "Delhi"
+// };
+
+// let choice = prompt ("Enter city or email");
+// console.log(profile[choice]);
+
+// Q2 .. Update the price of car to 4500000 and print updated Object
+car.price = 4500000;
+console.log(car.price); 
+
+//Q3.. Add new property mileage: 15 to the car object and print it.
+
+car.mileage = 15;
+console.log(car);
+
+// Q4..Delete the color property and print object.
+
+delete car.color;
+
+console.log(car);
+
+
+// Q5.. Check if property "price" exists using "price" in car and print result.
+console.log("price" in car); // gives true
+console.log("email" in car); // gives false
+
+// Add method showDetails() that prints: "BMW costs 4500000" Call the method.
+
+car.detail(); // calling method
+
+// Q7.. Add method applyDiscount() that reduces price by 10%.
+car.discount(); // it'll update the price
+
+car.detail(); // this will tell the updated price
+
+
