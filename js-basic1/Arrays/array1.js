2bb2c73c2cb0f9cf94fd3ebf226b0c6295ba42d6
ShivamMaletha21 <malethashivam43@gmail.com>
2026-02-20 08:06:10 +0530

    Learning Arrays

---
+++ js-basic1/Arrays/array1.js
@@ -0,0 +1,23 @@
+// Array Creation
+// There are 2 main ways
+
+// Method1: Using Square Bracket... ler arrayName = [value1,value2,value3];
+
+let fruits = ["Apple", "Mango","Orange"];
+console.log(fruits);
+
+
+// Method2: Using Array Constructor ... let arrayName = new Array (value1,value2);.. mostly avoid this
+let car = new Array("BMW","Jaguar");
+console.log(car);
+
+// Access values... arrayName[index]
+console.log(fruits[1]);
+
+// Change value
+fruits[1] = "Orange";
+console.log(fruits);
+
+
+
+
