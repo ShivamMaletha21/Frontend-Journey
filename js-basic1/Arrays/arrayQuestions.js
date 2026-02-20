5a259b71cf5db42c6a19aabe0615bab4d5dad3e7
ShivamMaletha21 <malethashivam43@gmail.com>
2026-02-20 15:13:13 +0530

    finished begineer questions based on array

--- js-basic1/Arrays/arrayQuestions.js
+++ js-basic1/Arrays/arrayQuestions.js
@@ -46,3 +46,88 @@ console.log(movies);
 // Q9.Check array length after adding new value.
 movies.push("khiladi2");
 console.log(movies);
+
+// Section 2: push()
+// Real life: Add item to shopping cart
+
+// Q10.. Create empty cart array.
+// Add "Shoes"
+let item = [];
+item.push("sneakers");
+item.push("loafers");
+item.push("heels");
+item.push("sandals");
+item.push("flats");
+item.push("ankle boots");
+
+console.log(item);
+console.log(item.length);
+
+// Section 3: pop()
+// Real life: Remove last item from cart
+
+// Q11.Remove last item from cart
+item.pop();
+console.log(item);
+
+// Q12..Check length after pop
+console.log(item.length);
+
+// Section 4: shift()
+
+// Q13.Remove first student
+item.shift();
+console.log(item);
+
+// Section 5: unshift()
+item.unshift("sneakers");
+console.log(item);
+
+// Looping (forEach)
+
+let student3 = ["anuj","amit","raskolnikov","rohan","tanish"];
+student3.forEach(function(value)
+{
+    console.log(value);
+
+});
+
+//Print numbers using forEach
+
+let number8 = [,44,33,22,33,44,44,55,33];
+number8.forEach((value)=>console.log(value));
+
+
+//Create array of 10 numbers
+//Print even index values
+//Print odd index values
+
+
+//Print even index values
+let number9 = [0,1,2,3,4,5,6,7,8,9];
+
+number9.forEach((num,index) => {
+    if (index % 2 == 0) {
+        console.log(num);
+    }
+
+}
+);
+
+//Print odd index values
+let number10 = [0,1,2,3,4,5,6,7,8,9];
+
+number10.forEach((num,index) => {
+    if (index % 2 !== 0) {
+        console.log(num);
+    }
+    
+}
+);
+
+
+
+
+
+
+
