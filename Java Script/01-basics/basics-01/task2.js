1ebf812d1a6d07217842da2be76d2a70931f8a58
ShivamMaletha21 <malethashivam43@gmail.com>
2026-04-29 08:59:55 +0530

    finished basic questions on js

---
+++ Java Script/01-basics/basics-01/task2.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("#btn");
+
+let para = document.querySelector("#text");
+
+btn.addEventListener("click",function () {
+
+para.textContent ="Hi,this is Shivam Maletha";
+});
