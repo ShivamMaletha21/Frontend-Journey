4ee677a82b73bd74b42291f0453b6a157d231d23
ShivamMaletha21 <malethashivam43@gmail.com>
2026-05-04 09:11:41 +0530

    completed basic calculator

--- Java Script/04-Loops/advance/calculator/script.js
+++ Java Script/04-Loops/advance/calculator/script.js
@@ -1,28 +1,60 @@
 let addBtn = document.getElementById("add");
 addBtn.addEventListener("click",addNumbers);
 
-// let sub = document.getElementById("sub");
-// sub.addEventListener("click",sub);
 
+let subBtn = document.getElementById("sub");
+subBtn.addEventListener("click", subtractNumbers);
 
-// let mul = document.getElementById("mul");
-// mul.addEventListener("click",mul);
+let mulBtn = document.getElementById("mult");
+mulBtn.addEventListener("click", multiplyNumbers);
 
-
-// let exit = document.getElementById("add");
-// exit.addEventListener("click",exit);
-
+let exitBtn = document.getElementById("exit");
+exitBtn.addEventListener("click",exitApp);
 
 
 
+// addition
 let sum = 0;
 function addNumbers () {
     
-let number = Number(document.getElementById("input").value);
+let number1 = Number(document.getElementById("input").value);
 
-sum = sum + number;
+sum = sum + number1;
 
 document.getElementById("result").innerText =  sum;
 
 document.getElementById("input").value = "";
+}
+
+
+// subtract
+
+let sub = 0;
+function subtractNumbers() {
+
+    let number2 = Number(document.getElementById("input").value);
+
+    sub = Math.abs(sub-number2);
+
+    document.getElementById("result").innerText = sub;
+    document.getElementById("input").value = "";
+}
+
+
+// multiply
+let mult = 1;
+
+function multiplyNumbers() {
+    let number3 = Number(document.getElementById("input").value);
+
+    mult= mult * number3;
+
+document.getElementById("result").innerText =  mult;
+
+document.getElementById("input").value = "";
+}
+// exit
+function exitApp() {
+    sum = 0;
+    document.getElementById("result").innerText = "";
 }
