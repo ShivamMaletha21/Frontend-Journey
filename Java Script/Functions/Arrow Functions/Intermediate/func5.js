fdb1e7b72db4ab94753bbfc34b41c8492ad05df7
ShivamMaletha21 <malethashivam43@gmail.com>
2026-06-06 16:59:35 +0530

    practising questions based on arrow function ques no.5

--- Java Script/Functions/Arrow Functions/Intermediate/func5.js
+++ Java Script/Functions/Arrow Functions/Intermediate/func5.js
@@ -1,1 +1,19 @@
 // Q5 Create a function that accepts a user's name and unlimited hobbies.
+
+const Realname = (userName, ...hobbies) => {
+
+    let result = document.getElementById("msg").innerText = `The user name is ${userName} and its hobbies are ${hobbies}`
+    return result;
+}
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click",() => {
+
+    let name = document.getElementById("name").value;
+
+    let hobbies = document.getElementById("hobbies").value;
+      let realhobbies = hobbies.split(" ");
+
+    let RealResult = Realname(name,...realhobbies);
+
+})
