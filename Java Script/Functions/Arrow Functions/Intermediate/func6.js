c23779f6a7fc4ecb9c86ecb3c94959936b633f5e
ShivamMaletha21 <malethashivam43@gmail.com>
2026-06-09 23:57:05 +0530

    finished ques6 based on arrow function

--- Java Script/Functions/Arrow Functions/Intermediate/func6.js
+++ Java Script/Functions/Arrow Functions/Intermediate/func6.js
@@ -2,12 +2,18 @@
 
 const product = (...product_names)=>{
 
-     return products.length;
+     return product_names.length;
 
     
 }
 
 let btn = document.getElementById("btn");
 btn.addEventListener("click",()=>{
-    
+
+    let prod_names = document.getElementById("name").value;
+    let real_names = prod_names.split(" ");
+
+    let prod_result = product(...real_names);
+
+    document.getElementById("msg").innerText = prod_result;
 })
