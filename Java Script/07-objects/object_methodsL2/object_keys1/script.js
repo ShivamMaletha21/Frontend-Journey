d1c05a32de1fabd2a76711b45b843273c15e2ced
ShivamMaletha21 <malethashivam43@gmail.com>
2026-06-27 23:17:56 +0530

    solved questions based on object.keys

--- Java Script/07-objects/object_methodsL2/object_keys1/script.js
+++ Java Script/07-objects/object_methodsL2/object_keys1/script.js
@@ -13,10 +13,38 @@ const student = {
   city: "Dehradun"
 };
 
-const keys = Object.keys(student);
+// Write a program to print only those keys whose length is greater than 5 characters.
+let result = "";
 
-for (let key of keys) {
+Object.keys(student).forEach((key) => {
   if (key.length > 5) {
-    console.log(key);
+    result += key + "<br>";
   }
-}
+});
+document.getElementById("display1").innerHTML = result;
+
+// Write a program to print all keys in uppercase.
+let result2 = "";
+Object.keys(student).forEach((keys)=>{
+ result2 += keys.toUpperCase() + "<br>";
+});
+document.getElementById("display2").innerHTML = result2;
+
+// Write a program to find the longest key in an object.
+let result3 = "";
+
+Object.keys(student).forEach((key) => {
+  if (key.length >  result3.length) {
+    result3 = key;
+  }
+});
+document.getElementById("display3").innerHTML = result3;
+
+// Write a program to find the shortest key in an object.
+let result4 = Object.keys(student)[0];
+Object.keys(student).forEach((key) => {
+  if (key.length <  result4.length) {
+    result4 = key;
+  }
+});
+document.getElementById("display4").innerHTML = result4;
