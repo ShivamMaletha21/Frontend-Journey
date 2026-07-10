ec010fdee0c9398ba49e71c89fd86f86268889f0
ShivamMaletha21 <malethashivam43@gmail.com>
2026-07-10 22:59:01 +0530

    object destructuring

---
+++ Java Script/07-objects/Obj_destructuring/script.js
@@ -0,0 +1,23 @@
+// array destrcuting
+let arr = ["Shivam", 20, "India"];
+
+let [name, age, country] = arr;
+
+console.log(name);    // Shivam
+console.log(age);     // 20
+console.log(country); // India
+
+
+
+// object destructuing
+let person = {
+    name: "Shivam",
+    age: 20,
+    city: "Dehradun"
+};
+
+let { name, age, city } = person;
+
+console.log(name); // Shivam
+console.log(age);  // 20
+console.log(city); // Dehradun
