57fed88e741b66c590c92c5662900c0ef5f0eec4
ShivamMaletha21 <malethashivam43@gmail.com>
2026-07-14 15:08:36 +0530

    learning destructuring in js

--- Java Script/07-objects/Obj_destructuring/script.js
+++ Java Script/07-objects/Obj_destructuring/script.js
@@ -1,23 +1,48 @@
-// array destrcuting
-let arr = ["Shivam", 20, "India"];
+// // array destrcuting
+// let arr = ["Shivam", 20, "India"];
 
-let [name, age, country] = arr;
+// let [name, age, country] = arr;
 
-console.log(name);    // Shivam
-console.log(age);     // 20
-console.log(country); // India
+// console.log(name);    // Shivam
+// console.log(age);     // 20
+// console.log(country); // India
 
 
 
-// object destructuing
-let person = {
-    name: "Shivam",
-    age: 20,
-    city: "Dehradun"
-};
+// // object destructuing
+// let person = {
+//     name: "Shivam",
+//     age: 20,
+//     city: "Dehradun"
+// };
 
-let { name, age, city } = person;
+// let { name, age, city } = person;
 
-console.log(name); // Shivam
-console.log(age);  // 20
-console.log(city); // Dehradun
+
+// console.log(name); // Shivam
+// console.log(age);  // 20
+// console.log(city); // Dehradun
+
+// eg of array destructuring
+// 1. skip values
+let arr2 = [10,20,30];
+let [a, , c] = arr2;
+console.log(a,c);
+
+//2. default avlue
+let arr3 = [10];
+let [d,e = 50] = arr3;
+console.log(d,e);
+
+//3. rest operator
+let arr4 = [1,2,3,4,5,6,7];
+let [f, ...rest] = arr4;
+
+// 4. swap 
+let g= 10;
+let h = 20;
+[g,h] = [h,g];
+console.log(g,h);
+
+
+
