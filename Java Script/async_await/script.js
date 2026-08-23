5901d56caedf0e3acadf6d134cc803d712209d83
ShivamMaletha21 <malethashivam43@gmail.com>
2026-08-23 18:13:29 +0530

    finished questions practise on async,await JS

--- Java Script/async_await/script.js
+++ Java Script/async_await/script.js
@@ -1,13 +1,72 @@
-async function first() {
-    return "Hello";
+// async function first() {
+//     return "Hello";
+// }
+
+// // this will not give output, as due to async the first become a async function 
+// // (note this is not asynchrnous function, it is asyn) which always returns a promise
+// // so here, return "hello" becomes -> return Promise.resolve("hello");
+
+// // let result = first(); , 
+// // console.log(result);
+
+// first().then(function(result){
+//     console.log(result);
+// })
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// // full eg of async/ await
+// function getData(){
+//     return new Promise ((resolve)=>{
+//         setTimeout(()=>{
+//            resolve("Hello");
+//         },2000);
+//     });
+// }
+
+// async function showData(){
+//     console.log("Start");
+
+//     // here we are calling getdata();
+//         const result = await getData();
+
+//     console.log(result);
+
+//     console.log("End");
+// }
+
+// showData();
+
+
+// error in await, that's why to handle error we will use try and catch
+// function getData() {
+//     return Promise.reject("Something went wrong");
+// }
+
+// async function showData() {
+//     const result = await getData();
+
+//     console.log(result);
+// }
+
+// showData();
+
+
+function getData() {
+    return Promise.reject("Something went wrong");
 }
 
-// this will not give output, as due to async the first become a asynchronous function which always returns a promise
-// so here, return "hello" becomes -> return Promise.resolve("hello");
+async function showData() {
+    try {
+        const result = await getData();
 
-// let result = first(); , 
-// console.log(result);
+        console.log(result);
+    }
+     catch (error) {
+        console.log(error);
+    }
+}
 
-first().then(function(result){
-    console.log(result);
-})
+showData();
