79e7e34755c2d67d0df4127c7ddc842e30da6b45
ShivamMaletha21 <malethashivam43@gmail.com>
2026-08-23 11:15:53 +0530

    finished questions on callbacks

--- Java Script/Callback/script.js
+++ Java Script/Callback/script.js
@@ -1,13 +1,13 @@
-// function first(callback) {
-//     console.log("First");
-//     callback();
-// }
+function first(callback) {
+    console.log("First");
+    callback();
+}
 
-// function second() {
-//     console.log("Second");
-// }
+function second() {
+    console.log("Second");
+}
 
-// first(second);
+first(second);
 
 // Level 1 — Basic Callback
 
@@ -16,17 +16,17 @@
 // The function should print a greeting message and then execute the callback
 
 
-// function greetUser(username,callback) {
+function greetUser(username,callback) {
 
-//     console.log("Hello" + " " +  username)
-//     callback();
+    console.log("Hello" + " " +  username)
+    callback();
 
-//     }  
-//       function user_msg() {
-//         console.log("hey this is shivam maletha");
+    }  
+      function user_msg() {
+        console.log("hey this is shivam maletha");
 
-// }
-// greetUser("Shivam",user_msg);
+}
+greetUser("Shivam",user_msg);
 
 
 // Q2. Welcome Message
@@ -36,15 +36,15 @@
 
 
 
-// function js_msg(callback) {
+function js_msg(callback) {
 
-//     console.log("Welcome to JS");
-// }
+    console.log("Welcome to JS");
+}
 
-// function execute_call() {
-//     callback();
-// }
-// js_msg(execute_call);
+function execute_call() {
+    callback();
+}
+js_msg(execute_call);
 
 
 
@@ -55,16 +55,16 @@
 
 
 
-// function addition(num1, num2, callback){
+function addition(num1, num2, callback){
 
-//    callback(num1, num2);
+   callback(num1, num2);
 
-// }
-// function execute_call2(a,b) {
-//    console.log(`the addition of two  ${a} and ${b} numbers is ${a + b}`);
-// }
+}
+function execute_call2(a,b) {
+   console.log(`the addition of two  ${a} and ${b} numbers is ${a + b}`);
+}
 
-// addition(30,20,execute_call2);
+addition(30,20,execute_call2);
 
 
 
@@ -74,15 +74,15 @@
 // The callback should multiply the numbers and display the result.
 
 
-// function multiply(num1, num2,callback) {
-//     callback(num1,num2);
-// }
+function multiply(num1, num2,callback) {
+    callback(num1,num2);
+}
 
-// function execute_call(a,b) {
-//     console.log(`the multiplication of two numbers ${a} and ${b} is ${a * b}`);
-// }
+function execute_call(a,b) {
+    console.log(`the multiplication of two numbers ${a} and ${b} is ${a * b}`);
+}
 
-// multiply(20,30,execute_call);
+multiply(20,30,execute_call);
 
 
 // Level 2 — Callback with Different Operations
@@ -96,67 +96,67 @@
 // Division
 // The calculator should perform the operation provided through the callback.
 
-// function calculator(num1,num2,callback){
+function calculator(num1,num2,callback){
 
-//     // use them as callback
-//     callback(num1,num2);
+    // use them as callback
+    callback(num1,num2);
 
-// }
+}
 
 
-// // addition
-// function addition(a,b){
+// addition
+function addition(a,b){
 
-//     console.log(`the addition of number ${a} and ${b} is ${a + b}`);
-// }
+    console.log(`the addition of number ${a} and ${b} is ${a + b}`);
+}
 
-// calculator(10,20,addition);
+calculator(10,20,addition);
 
-// // Subtraction
-// function Subtraction(a,b) {
+// Subtraction
+function Subtraction(a,b) {
     
-//      console.log(`the Subtraction of number ${a} and ${b} is ${a - b}`);
-// }
-// calculator(30,20,Subtraction);
+     console.log(`the Subtraction of number ${a} and ${b} is ${a - b}`);
+}
+calculator(30,20,Subtraction);
 
 
-// // Multiplication
-// function Multiplicaton(a,b) {
+// Multiplication
+function Multiplicaton(a,b) {
     
-//      console.log(`the multiplication of number ${a} and ${b} is ${a * b}`);
-// }
-// calculator(30,20,Multiplicaton);
+     console.log(`the multiplication of number ${a} and ${b} is ${a * b}`);
+}
+calculator(30,20,Multiplicaton);
 
 
-// // Division
-// function Divison(a,b) {
+// Division
+function Divison(a,b) {
     
-//      console.log(`the divison of number ${a} and ${b} is ${a / b}`);
-// }
-// calculator(30,20,Divison);
+     console.log(`the divison of number ${a} and ${b} is ${a / b}`);
+}
+calculator(30,20,Divison);
 
 
 // Q6. Even or Odd Using Callback
 // WAP to create a function that accepts a number and a callback.
 // The callback should determine whether the number is even or odd.
 
-// function even_odd(num,callback) {
+function even_odd(num,callback) {
 
-//     callback(num);
+    callback(num);
 
-// }
+}
 
-// function even(a) {
+function even(a) {
 
-//     if (a % 2 === 0 ) {
-//         console.log(`the number is even ${a}`);
-//     }
+    if (a % 2 === 0 ) {
+        console.log(`the number is even ${a}`);
+    }
 
-//     else {
-//         console.log(`the number is odd ${a}`);
-//     }
-// }
-// even_odd(3,even);
+    else {
+        console.log(`the number is odd ${a}`);
+    }
+}
+even_odd(3,even);
 
 
 // Q7. Positive or Negative Using Callback
@@ -166,25 +166,25 @@
 // Negative
 // Zero
 
-// function postive_negative(number,callback) {
+function postive_negative(number,callback) {
 
-//     callback(number);
-// }
-// function positive(a) {
-//     if (a>0)
-//     {
-//         console.log(`the number ${a} is +ve`);
-//     }
+    callback(number);
+}
+function positive(a) {
+    if (a>0)
+    {
+        console.log(`the number ${a} is +ve`);
+    }
 
-//     else if (a<0){
-//          console.log(`the number ${a} is -ve`);
-//     }
-//      else  {
+    else if (a<0){
+         console.log(`the number ${a} is -ve`);
+    }
+     else  {
 
-//          console.log(`the number ${a} is 0`);
-//      }
-// }
-// postive_negative(-4,positive);
+         console.log(`the number ${a} is 0`);
+     }
+}
+postive_negative(-4,positive);
 
 
 
@@ -195,13 +195,13 @@
 
 
 
-// function user(name,callback) {
-//     callback(name);
-// }
-// function register(a) {
-//     console.log(`the username ${a} is registered successfully`);
-// }
-// user("Shivam",register);
+function user(name,callback) {
+    callback(name);
+}
+function register(a) {
+    console.log(`the username ${a} is registered successfully`);
+}
+user("Shivam",register);
 
 
 // Q17. Student Processing System
@@ -209,7 +209,7 @@
 // Student information is received.
 // A callback calculates the total marks.
 // Another callback calculates the percentage.
-.
+
 // Use callback functions to perform each operation.
 
 function student(marks1,marks2,marks3,marks4,marks5,callback){
@@ -234,6 +234,7 @@ student(10,20,30,40,50,percentage);
 student(10,20,30,40,50,marks);
 
 
+
 // Q21. Array Processor 
 // WAP to create a function that accepts an array and a callback.
 // The callback should perform an operation on every element of the array.
@@ -242,3 +243,18 @@ student(10,20,30,40,50,marks);
 // Square every number
 // Add 10 to every number
 // Check whether each number is even
+function array1(array,callback){
+
+  for (let i  = 0; i< array.length;i++){
+    callback(array[i]);
+  }
+}
+
+  console.log("double array");
+function double(a) {
+
+  
+    console.log(` ${2 * a}`);
+}
+
+array1([1,2,3,4,5],double);
